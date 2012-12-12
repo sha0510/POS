1fc9bfc66ba20dd3923ce571cff208bbedcdd020
Shaibal Barua <shaibalit2004@gmail.com>
2012-12-12 19:44:41 +0100

    DepartmentView changed

---
+++ POS System/POS/BusinessLogic/BLLClasses/DepartmentBLL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DAL.Datamodel;
+using DAL.DALClasses;
+
+namespace BusinessLogic.BLLClasses
+{
+    public class DepartmentBLL
+    {
+        DepartmentDAL departmentDAL;
+
+        public void addNewData(department newData)
+        {
+            departmentDAL = DepartmentDAL.getInstance();
+
+            department existingData = departmentDAL.findByName(newData);
+
+            if (existingData == null)
+            {
+                departmentDAL.insert(newData);
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
